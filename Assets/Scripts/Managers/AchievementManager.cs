20450b27bb90536ff185f42ae6a5fcedbe769998
Kirushanth Aruldas <kirushanth@live.ca>
2018-11-14 18:23:56 -0500

    - Achievements in progress....     - Saveing and loading so far.

--- Assets/Scripts/Managers/AchievementManager.cs
+++ Assets/Scripts/Managers/AchievementManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public enum ACHIEVEMENT_LIST {
 	TAP_COUNT, CHEST_COUNT, LOOT_COUNT, SCENES_COUNT, UPGRADES_COUNT, CURRENCY_COUNT,
@@ -11,15 +9,57 @@ public enum ACHIEVEMENT_LIST {
 	TAP_POWER
 }
 
-public class AchievementManager : MonoBehaviour {
+[System.Serializable]
+public struct Achievement {
+	public ACHIEVEMENT_LIST achievementIndex;
+	public int achievementLevel;
+	public int currentAchievementProgress;
+	public int nextAchievementTarget;
+}
 
-	// Use this for initialization
-	void Start () {
-		
+public class AchievementManager : MonoBehaviour {
+	public Achievement[] achievements;
+
+	private int m_numAchievements = 11;
+
+	void Awake() {
+		CreateAchievementsList();
+		CheckAchievements();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void CreateAchievementsList() {
+		achievements = new Achievement[m_numAchievements];
+
+		for (int i = 0; i < m_numAchievements; i++) {
+			achievements[i].achievementIndex = (ACHIEVEMENT_LIST)i;
+			achievements[i].achievementLevel = 0;
+		}
+	}
+
+	private void CheckAchievements() {
+		if (PlayerPrefs.HasKey("AchievementsInited")) {
+			LoadAchievements();
+		} else {
+			SaveInitialAchievements();
+		}
+	}
+
+	private void LoadAchievements() {
+		for (int i = 0; i < m_numAchievements; i++) {
+			achievements[i].achievementLevel = PlayerPrefs.GetInt(((ACHIEVEMENT_LIST)i).ToString() + "_Level");
+			achievements[i].currentAchievementProgress = PlayerPrefs.GetInt(((ACHIEVEMENT_LIST)i).ToString() + "_CurrProg");
+			achievements[i].nextAchievementTarget = PlayerPrefs.GetInt(((ACHIEVEMENT_LIST)i).ToString() + "_Target");
+		}
+	}
+
+	private void SaveInitialAchievements() {
+		PlayerPrefs.SetInt("AchievementsInited", 1);
+		for (int i = 0; i < m_numAchievements; i++) {
+			PlayerPrefs.SetInt(((ACHIEVEMENT_LIST)i).ToString() + "_Level", 0);
+			PlayerPrefs.SetInt(((ACHIEVEMENT_LIST)i).ToString() + "_CurrProg", 0);
+			PlayerPrefs.SetInt(((ACHIEVEMENT_LIST)i).ToString() + "_Target", 0);
+		}
+
+		// TODO: Set the  intial target 
 	}
 }
