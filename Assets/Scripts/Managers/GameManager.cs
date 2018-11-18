d3b4de6e2ba9029c38316f4cc79a04ce9b5cbdf8
Zeeshan Mohammed <zee_shan265@hotmail.com>
2018-11-18 02:12:30 -0500

    a more finite state machine at work, random loot showing up from one of the tiers

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EVENT_SYSTEM {
-	LOADING_SCREEN,				//Show loading screen, load all vars and assets
-	DAMAGE_PHASE,				//Chest will spawn and player can begin tapping
-	REWARD_SPAWN				//Rewards spawn and player earns it
-}
-
 public class GameManager : MonoBehaviour {
-	public EVENT_SYSTEM eventSystem;
+	public enum State {
+		LOADING_SCREEN,	SPAWN_CHEST, DAMAGE_PHASE, DESPAWN_CHEST, REWARD_SPAWN
+	}
+
+	public State currentState;
 	public bool isMenuOpen = false;
 
 	[Header("Loot Box Properties")]
@@ -36,92 +34,104 @@ public class GameManager : MonoBehaviour {
 		m_lootManager = GetComponent<LootManager>();
 		m_hudController = GameObject.Find("HUD").GetComponent<HUDController>();
 
-		//FIXME: This if function will be placed in an event system which will check through
-		// the save file to see what the player's EXP is.
-		if(currentXP == 0) {
-			chestsOpened = 0;
-			earnings = 0;
-			level = 0;
-			maxXP = 25f;
-			tapDamage = 0.75f;
-			idleDamage = tapDamage * 1.45f;
+		currentState = State.LOADING_SCREEN;
+	}
+
+	public void SetState(State newState) {
+		switch(currentState) {
+			case State.LOADING_SCREEN:
+				newState = State.SPAWN_CHEST;
+			break;
+			case State.SPAWN_CHEST:
+				newState = State.DAMAGE_PHASE;
+			break;
+			case State.DAMAGE_PHASE:
+				newState = State.DESPAWN_CHEST;
+			break;
+			case State.DESPAWN_CHEST:
+				newState = State.REWARD_SPAWN;
+			break;
+			case State.REWARD_SPAWN:
+				newState = State.DAMAGE_PHASE;
+			break;
 		}
-
-		// Debug.Log("Amount of broken items: " + m_lootManager.brokenItems.lootItem.Length);
-
-		SimplePool.Preload(chestPrefab, 1);
-		SimplePool.Spawn(chestPrefab, new Vector3(0,-1.5f,0), transform.rotation);
-
-		eventSystem = EVENT_SYSTEM.LOADING_SCREEN;
+		currentState = newState;
 	}
 
 	void Update() {
-		// Debug.Log(isDead);
-		switch(eventSystem) {
-			case EVENT_SYSTEM.LOADING_SCREEN:
-				currentHP = maxHP;
+		//TODO:Check and load in assets here to switch to next state for now it happens through key press
+		if(currentState == State.LOADING_SCREEN && Input.GetKeyDown(KeyCode.X)) {
+			//FIXME: This if function will be placed in an event system which will check through
+			// the save file to see what the player's EXP is.
+			if(currentXP == 0) {
+				chestsOpened = 0;
+				earnings = 0;
+				level = 0;
+				maxXP = 25f;
+				tapDamage = 0.75f;
+				idleDamage = tapDamage * 1.45f;
+			}
+			// Debug.Log("Amount of broken items: " + m_lootManager.brokenItems.lootItem.Length);
+			SimplePool.Preload(chestPrefab, 1);
+			SetState(State.SPAWN_CHEST);
+		} else if(currentState == State.SPAWN_CHEST) {
+			SimplePool.Spawn(chestPrefab, new Vector3(0,-1.5f,0), transform.rotation);
+			SetState(State.DAMAGE_PHASE);
+		} else if(currentState == State.DAMAGE_PHASE) {
+			roundedHP = System.Math.Round(currentHP, 2);
+			if(currentHP <= 0) {
+				currentHP = 0; 
+				isDead = true;
+			}
+
+			if(currentTimer <= 0) { 
 				currentTimer = maxTimer;
+				currentHP = maxHP;
+				isDead = false;
+			}
 
-				eventSystem = EVENT_SYSTEM.DAMAGE_PHASE;
-			break;
-
-			case EVENT_SYSTEM.DAMAGE_PHASE:
-				roundedHP = System.Math.Round(currentHP, 2);
-				if(roundedHP <= 0) {
-					currentHP = 0; 
-					isDead = true;
-					chestsOpened += 1;
-					earnings += 15;
-					currentXP += 25f;
-					maxHP += 25f;
-					maxTimer -= 0.135f;
-					currentHP = maxHP;
-					currentTimer = maxTimer;
-					eventSystem = EVENT_SYSTEM.REWARD_SPAWN;
-				}
-
-				if(currentTimer <= 0) { 
-					currentTimer = maxTimer;
-					currentHP = maxHP;
-					isDead = false;
-				}
-
-				if(!isDead) {
-					currentHP -= idleDamage * Time.deltaTime;
-					currentTimer -= Time.deltaTime;
-					if(currentTimer != 0) {
-						if(Input.GetMouseButtonDown(0) && !isMenuOpen) {
-							currentHP -= tapDamage;
-						}
-						if(Input.GetKeyDown(KeyCode.Space)) {
-							currentHP -= tapDamage * 5;
-						}
+			if(!isDead) {
+				currentHP -= idleDamage * Time.deltaTime;
+				currentTimer -= Time.deltaTime;
+				if(currentTimer != 0) {
+					if(Input.GetMouseButtonDown(0) && !isMenuOpen) {
+						currentHP -= tapDamage;
+					}
+					if(Input.GetKeyDown(KeyCode.Space)) {
+						currentHP -= tapDamage * 5;
 					}
 				}
-			break;
-
-			case EVENT_SYSTEM.REWARD_SPAWN:
-				if(currentXP >= maxXP) {
-					float remainderXp = currentXP - maxXP;
-					currentXP = 0;
-					currentXP += remainderXp;
-					maxXP += 5.5f;
-					level += 1;
-					earnings += 20;
-					tapDamage += 0.15f;
-					idleDamage = tapDamage * 1.45f;
-				}
-				StartCoroutine("SpawnChest");
-			break;
+			} else {
+				SetState(State.DESPAWN_CHEST);
+			}
+		} else if(currentState == State.DESPAWN_CHEST) {
+			maxHP += 25f;
+			maxTimer -= 0.135f;
+			currentHP = maxHP;
+			currentTimer = maxTimer;
+			SimplePool.Despawn(chestPrefab);
+			for(int i = 0; i < m_lootManager.loot.Length; i++) {
+				m_lootManager.loot[i].SetActive(true);
+			}
+			SetState(State.REWARD_SPAWN);
+		} else if(currentState == State.REWARD_SPAWN) {
+			earnings += 15;
+			currentXP += 25f;
+			chestsOpened += 1;
+			if(currentXP >= maxXP) {
+				float remainderXp = currentXP - maxXP;
+				currentXP = 0;
+				currentXP += remainderXp;
+				maxXP += 5.5f;
+				level += 1;
+				earnings += 20;
+				tapDamage += 0.15f;
+				idleDamage = tapDamage * 1.45f;
+			}
+			isDead = false;
+			SetState(State.SPAWN_CHEST);
 		}
 
-	}
-
-	IEnumerator SpawnChest() {
-		SimplePool.Despawn(chestPrefab);
-		yield return new WaitForSeconds(0.15f);
-		isDead = false;
-		eventSystem = EVENT_SYSTEM.DAMAGE_PHASE;
 	}
 
 }
---
+++ Assets/Scripts/Managers/LootManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LootType {
+	public LootItemList itemList;
+	public float itemExp;
+	public Material itemMat;
+	public Animation itemAnimation;
+}
+
+public class LootManager : MonoBehaviour {
+	public LootType[] lootTypes;
+	public LootItemList lootItemList;
+
+	[Header("RNG Properties")]
+	// public GameObject lootPrefab;
+	public GameObject[] loot;
+	// public int randomLootIndex0;
+	// public int randomLootIndex1;
+	// public int randomLootIndex2;
+	[SerializeField] public int[] randomLoot;
+
+	[SerializeField] private float lootTimer, resetLootTimer;
+
+	GameManager m_gameManager;
+
+	void Awake() {
+		m_gameManager = GetComponent<GameManager>();
+		for(int i = 0; i < loot.Length; i++) {
+			loot[i].SetActive(false);
+		}
+		lootTimer = 5f;
+		resetLootTimer = lootTimer;
+	}
+
+	void Update() {
+		if(m_gameManager.roundedHP == m_gameManager.maxHP) {
+			PickLoot(lootItemList);
+		}
+
+		for(int i = 0; i < loot.Length; i++) {
+			if(loot[i].activeSelf) {
+				lootTimer -= Time.deltaTime;
+				if(lootTimer <= 0) {
+					loot[0].SetActive(false);
+					loot[1].SetActive(false);
+					loot[2].SetActive(false);
+					lootTimer = resetLootTimer;
+				}
+			}
+		}
+	}
+	
+	public void PickLoot(LootItemList lootCategory) {
+		randomLoot[0] = Random.Range(0, lootCategory.lootItem.Length);
+		randomLoot[1] = Random.Range(0, lootCategory.lootItem.Length);
+		randomLoot[2] = Random.Range(0, lootCategory.lootItem.Length);
+
+		loot[0].name = lootCategory.lootItem[randomLoot[0]].itemName;
+		loot[0].GetComponent<MeshRenderer>().material = lootCategory.lootItem[randomLoot[0]].lootMat;
+		// lootCategory.lootItem[i].exp = lootCategory.lootItem[randomLoot[0]].exp;
+		// lootCategory.lootItem[i].lootMat = lootCategory.lootItem[randomLoot[0]].lootMat;
+		// lootCategory.lootItem[i].lootAnimation = lootCategory.lootItem[randomLoot[0]].lootAnimation;
+
+		loot[1].name = lootCategory.lootItem[randomLoot[1]].itemName;
+		loot[1].GetComponent<MeshRenderer>().material = lootCategory.lootItem[randomLoot[1]].lootMat;
+		// lootCategory.lootItem[i].exp = lootCategory.lootItem[randomLoot[1]].exp;
+		// lootCategory.lootItem[i].lootMat = lootCategory.lootItem[randomLoot[1]].lootMat;
+		// lootCategory.lootItem[i].lootAnimation = lootCategory.lootItem[randomLoot[1]].lootAnimation;
+
+		loot[2].name = lootCategory.lootItem[randomLoot[2]].itemName;
+		loot[2].GetComponent<MeshRenderer>().material = lootCategory.lootItem[randomLoot[2]].lootMat;
+		// lootCategory.lootItem[i].exp = lootCategory.lootItem[randomLoot[2]].exp;
+		// lootCategory.lootItem[i].lootMat = lootCategory.lootItem[randomLoot[2]].lootMat;
+		// lootCategory.lootItem[i].lootAnimation = lootCategory.lootItem[randomLoot[2]].lootAnimation;
+
+		// for(int i = 0; i < commonItems.lootItem.Length; i++) {
+		// 	commonItems.lootItem[i].exp = commonExp;
+		// 	commonItems.lootItem[i].lootMat = commonMat;
+		// 	commonItems.lootItem[i].lootAnimation = commonAnimation;
+		// }
+		// for(int i = 0; i < uncommonItems.lootItem.Length; i++) {
+		// 	uncommonItems.lootItem[i].exp = uncommonExp;
+		// 	uncommonItems.lootItem[i].lootMat = uncommonMat;
+		// 	uncommonItems.lootItem[i].lootAnimation = uncommonAnimation;
+		// }
+		// for(int i = 0; i < rareItems.lootItem.Length; i++) {
+		// 	rareItems.lootItem[i].exp = rareExp;
+		// 	rareItems.lootItem[i].lootMat = rareMat;
+		// 	rareItems.lootItem[i].lootAnimation = rareAnimation;
+		// }
+		// for(int i = 0; i < legendaryItems.lootItem.Length; i++) {
+		// 	legendaryItems.lootItem[i].exp = legendaryExp;
+		// 	legendaryItems.lootItem[i].lootMat = legendaryMat;
+		// 	legendaryItems.lootItem[i].lootAnimation = legendaryAnimation;
+		// }
+		// for(int i = 0; i < primeItems.lootItem.Length; i++) {
+		// 	primeItems.lootItem[i].exp = primeExp;
+		// 	primeItems.lootItem[i].lootMat = primeMat;
+		// 	primeItems.lootItem[i].lootAnimation = primeAnimation;
+		// }
+	}
+
+}
