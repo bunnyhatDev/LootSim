6755665de57f020a0730057631da35889adbf2dc
Kirushanth Aruldas <thzgreywolf@gmail.com>
2018-11-18 17:10:35 -0500

    Its all back.. i think....

---
+++ Assets/Scripts/UI/StatCard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatCard : MonoBehaviour {
+	public Image image; // TODO:
+	public TextMeshProUGUI cardName;
+	public TextMeshProUGUI desc;
+}
